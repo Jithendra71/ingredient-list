44e2e7e0dfcfb802a021d3cf26d69ad52a67c878
Gowtham <gowtham.mandla1@gmail.com>
2022-01-04 19:42:19 +0530

    gowtham -2

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import Auth  from './components/Auth';
 import Ingredients from './components/Ingredients/Ingredients';
 import { AuthContext } from './context/auth-context';
 
+const string = 'jithendra'
+console.log(string)
+
+
 const App = props => {
   const authContext = useContext(AuthContext)
 
+  
   if(authContext.isAuth)
   {return <Ingredients />}
   return<Auth/>
